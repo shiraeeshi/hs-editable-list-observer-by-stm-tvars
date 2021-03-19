1b870e8df7c05d880f634b0ba057efcc8dbcf77a
shiraeeshi <shiraeeshi@mail.ru>
2021-03-19 09:21:47 +0600

    STM: move state to TVars, start listeners in their own threads (breaks drawing on screen)

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,11 @@ module Main where
 import System.IO (stdin, hSetEcho, hSetBuffering, hReady, BufferMode (NoBuffering) )
 import ViewUtils (clearScreen, showInRectangle, showInGrid)
 import Control.Monad (when)
+import Control.Concurrent.STM.TVar (TVar, newTVar, readTVar, writeTVar)
+import Control.Monad.STM (atomically, retry)
+import Control.Concurrent (forkIO)
 
-data RowData = Row { smth :: String }
+data RowData = Row { smth :: String } deriving Eq
 
 initialRows = [
   Row "something a"
@@ -14,53 +17,98 @@ initialRows = [
   , Row "something e"
   ]
 
+data AppStateData = AppState
+  { mainRows :: TVar [RowData]
+  , highlightedRowIndex :: TVar (Maybe Int)
+  , debugMessages :: TVar [RowData] }
+
 main :: IO ()
 main = do
   hSetBuffering stdin NoBuffering
   hSetEcho stdin False
-  loop Nothing []
+  (AppState mainRowsTV highlightedRowIndexTV debugMessagesTV) <- atomically $ do
+    mainRows <- newTVar initialRows
+    debugMessages <- newTVar ([] :: [RowData])
+    highlightedRowIndex <- newTVar Nothing
+    return $ AppState mainRows highlightedRowIndex debugMessages
+  forkIO $ do
+    let loop rows activeCellY = do
+          let activeCellCoords = fmap (\y -> (0, y)) activeCellY
+          showInGrid
+            xUpperLeft
+            yUpperLeft
+            columnCount
+            columnWidth
+            activeCellCoords
+            (map (\row -> [smth row]) rows)
+          (newMainRows, newActiveCellY) <- atomically $ do
+            newMainRows <- readTVar mainRowsTV
+            newActiveCellY <- readTVar highlightedRowIndexTV
+            if newMainRows == rows && newActiveCellY == activeCellY
+              then retry
+              else return (newMainRows, newActiveCellY)
+          loop newMainRows newActiveCellY
+    (mainRows, activeCellY) <- atomically $ do
+      rows <- readTVar mainRowsTV
+      y <- readTVar highlightedRowIndexTV
+      return (rows, y)
+    loop mainRows activeCellY
+  forkIO $ do
+    let loop debugMessages = do
+          showInGrid
+            xUpperLeft
+            (yUpperLeft+12)
+            columnCount
+            columnWidth
+            Nothing
+            (map (\row -> [smth row]) debugMessages)
+          newDebugMessages <- atomically $ do
+            newDebugMessages <- readTVar debugMessagesTV
+            if newDebugMessages == debugMessages
+              then retry
+              else return newDebugMessages
+          loop newDebugMessages
+    debugMessages <- atomically $ readTVar debugMessagesTV
+    loop debugMessages
+      
+  clearScreen
+  keepListeningToKeyPresses mainRowsTV highlightedRowIndexTV debugMessagesTV
   where
     xUpperLeft = 0
     yUpperLeft = 0
     columnCount = 1
     columnWidth = 14
     rowCount = length initialRows
-    loop :: Maybe (Int, Int) -> [RowData] -> IO ()
-    loop activeCellCoords debugRows = do
-      clearScreen
-      showInGrid
-        xUpperLeft
-        (yUpperLeft+12)
-        columnCount
-        columnWidth
-        Nothing
-        (map (\row -> [smth row]) debugRows)
-      showInGrid
-        xUpperLeft
-        yUpperLeft
-        columnCount
-        columnWidth
-        activeCellCoords
-        (map (\row -> [smth row]) initialRows)
+    keepListeningToKeyPresses mainRowsTV highlightedRowIndexTV debugMessagesTV = do
       key <- getKey
       when (key /= "\ESC") $ do
         case key of
           "\ESC[A" -> do -- up
-            let newActiveCellCoords =
-                  case activeCellCoords of
-                    Just (x, y) -> Just (x, max 0 (y-1))
-                    Nothing -> Just (0, 0)
-                debugRow = Row $ "up, " ++ show(fmap snd newActiveCellCoords)
-                newDebugRows = take 5 (debugRow:debugRows)
-            loop newActiveCellCoords newDebugRows
+            atomically $ do
+              activeCellY <- readTVar highlightedRowIndexTV
+              debugRows <- readTVar debugMessagesTV
+              let newActiveCellY =
+                    case activeCellY of
+                      Just y -> Just $ max 0 (y-1)
+                      Nothing -> Just 0
+                  debugRow = Row $ "up, " ++ show(newActiveCellY)
+                  newDebugRows = take 5 (debugRow:debugRows)
+              writeTVar highlightedRowIndexTV newActiveCellY
+              writeTVar debugMessagesTV newDebugRows
+            keepListeningToKeyPresses mainRowsTV highlightedRowIndexTV debugMessagesTV
           "\ESC[B" -> do -- down
-            let newActiveCellCoords =
-                  case activeCellCoords of
-                    Just (x, y) -> Just (x, min (rowCount-1) (y+1))
-                    Nothing -> Just (0, 0)
-                debugRow = Row $ "down, " ++ show(fmap snd newActiveCellCoords)
-                newDebugRows = take 5 (debugRow:debugRows)
-            loop newActiveCellCoords newDebugRows
+            atomically $ do
+              activeCellY <- readTVar highlightedRowIndexTV
+              debugRows <- readTVar debugMessagesTV
+              let newActiveCellY =
+                    case activeCellY of
+                      Just y -> Just $ min (rowCount-1) (y+1)
+                      Nothing -> Just 0
+                  debugRow = Row $ "down, " ++ show(newActiveCellY)
+                  newDebugRows = take 5 (debugRow:debugRows)
+              writeTVar highlightedRowIndexTV newActiveCellY
+              writeTVar debugMessagesTV newDebugRows
+            keepListeningToKeyPresses mainRowsTV highlightedRowIndexTV debugMessagesTV
           "\n" -> do -- enter
             return ()
           "q" -> return ()
